ff215f63e217af625f8403fbe90fa555f58d13e1
Cherinho11 <marcellagrau6@gmail.com>
2026-06-23 15:05:25 -0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,12 @@
+let areaDaParede // entrada - input
+let litrosDeTinta // saída - output
+
+areaDaParede = prompt("Informe a área da parede em metros quadrados");
+
+areaDaParede = parseFloat(areaDaParede);
+
+litrosDeTinta = parseFloat(areaDaParede) / 5;
+
+alert("A quantidade de litros de tinta necessária para pintar a parede é de "+ litrosDeTinta)
+% 3 == 0
+% 5 == 0
